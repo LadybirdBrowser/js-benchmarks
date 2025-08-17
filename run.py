387c9d1e0843065345eea65215f305be6c4dbe28
Timothy Flynn <trflynn89@pm.me>
2025-08-17 11:29:11 -0400

    Add missing argument to run_benchmark for warmup runs

--- run.py
+++ run.py
@@ -54,7 +54,7 @@ def main():
             for test_file in sorted(os.listdir("SunSpider")):
                 if not test_file.endswith(".js"):
                     continue
-                run_benchmark(args.executable, "SunSpider", test_file, 1, 0, 0, suppress_output=True)
+                run_benchmark(args.executable, [], "SunSpider", test_file, 1, 0, 0, suppress_output=True)
 
     results = {}
     table_data = []
